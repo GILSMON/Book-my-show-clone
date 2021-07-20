fb019c7a98e83b8cde1be50df72a8ceabca5048f
Gilsmon <gilscherian13@gmail.com>
2021-07-20 15:21:37 +0530

    Navbar and corousel

---
+++ planning.js
@@ -0,0 +1,27 @@
+// Book my show planning
+
+// we need a reuseable carousal  
+//       -> Hero carousal
+//       -> common carousal
+//       -> cast and crew small carousal
+
+
+// Pages
+// -> Home page
+// -> Movie page
+// -> Categories page
+// -> Auth page
+// -> Razorpay payment gateway
+
+// Plan -> HOW are we building it ????
+// Layouts
+// Routes
+// Pages
+// Components
+
+// HOC
+// Higher Order Components -> return a component(function component)
+
+
+
+
